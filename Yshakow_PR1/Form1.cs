aef57c03ea5c8c0659fdfd5fe4df9ce2ed8fb7bc
mambabusin142@mail.ru <mambabusin142@mail.ru>
2022-09-22 16:16:00 +1000

    button1.Text = "Hello";

--- Yshakow_PR1/Form1.cs
+++ Yshakow_PR1/Form1.cs
@@ -16,5 +16,10 @@ namespace Yshakow_PR1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Hello";
+        }
     }
 }
